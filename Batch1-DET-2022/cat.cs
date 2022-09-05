44012fb54c269d27f4ff6dcaaf9f117c727b226e
indrajeet <indrajeetkhot17@gmail.com>
2022-09-05 19:13:28 +0530

    cat class added

---
+++ Batch1-DET-2022/cat.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Batch1_DET_2022
+//{
+//    public class cat : Animal
+//    {
+//        public override string ToString()
+//        {
+//            return "meow-meow";
+//        }
+//    }
+//}

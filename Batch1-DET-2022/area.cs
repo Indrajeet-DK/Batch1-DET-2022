c11c37ff1c46f7eb7e8f79ba294c7dee74c81bcb
indrajeet <indrajeetkhot17@gmail.com>
2022-09-05 19:06:46 +0530

    area class  added

---
+++ Batch1-DET-2022/area.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Batch1_DET_2022
+//{
+//    internal class area
+//    {
+
+//        //        int redius;
+
+//        //        public area(int circleredius)
+//        //        {
+//        //            redius = circleredius;
+//        //        }
+
+//        //        public double GetArea()
+//        //        {
+//        //            return 3.142 * redius * redius;
+//        //        }
+//    }
+//}

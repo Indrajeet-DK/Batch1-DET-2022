e863206eeb59a57285cc502efd819389612403dd
indrajeet <indrajeetkhot17@gmail.com>
2022-09-05 19:54:49 +0530

    new class added

---
+++ Batch1-DET-2022/Person.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Batch1_DET_2022
+//{
+//    internal class person
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("Enter age:");
+//            int age = Convert.ToInt32(Console.ReadLine());
+//            try
+//            {
+
+//                if (age > 120)
+//                {
+//                    throw new InvalidAgeException("Please check the entered age:");
+//                }
+//                else
+//                {
+//                    Console.WriteLine($"Age is {age}");
+//                }
+
+//            }
+//            catch (InvalidAgeException i)
+//            {
+//                Console.WriteLine(i.Message);
+
+
+//            }
+//            catch (Exception e)
+//            {
+
+//            }
+//        }
+//    }
+//}
---
+++ Batch1-DET-2022/TestEmployee.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Batch1_DET_2022
+//{
+//    internal class testEmployee
+//    {
+//        public static void Main()
+//        {
+//            employee emp = employee(22830, "indru", "belgum", 7656, 8674765, 33500);
+//            Console.WriteLine($"employee sal{emp.GetNetSalary()}");
+//            Console.WriteLine($"employee sal{emp.Get()}");
+//        }
+//    }
+//}

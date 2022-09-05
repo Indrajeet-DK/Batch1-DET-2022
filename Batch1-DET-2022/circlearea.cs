2c05cde788799e1d7656e69a6075254850940aa8
indrajeet <indrajeetkhot17@gmail.com>
2022-09-05 19:16:12 +0530

    circlearea class added

---
+++ Batch1-DET-2022/circlearea.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Batch1_DET_2022
+//{
+//    internal class circlearea
+//    {
+
+        //        public static void Main()
+        //        {
+        //            area a = new area(50);
+        //            Console.WriteLine($"the area of the circle is {a.GetArea()} cm square");
+
+        //        }
+//    }
+//}
